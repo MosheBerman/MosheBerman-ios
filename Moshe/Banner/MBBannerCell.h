e7c567a30114bb377ddf4251ad0ec13b934f03b5
Moshe Berman <moshberm@gmail.com>
2013-04-25 22:00:05 -0400

    Added an image to the banner cell.

---
+++ Moshe/Banner/MBBannerCell.h
@@ -0,0 +1,13 @@
+//
+//  MBBannerCell.h
+//  Moshe
+//
+//  Created by Moshe Berman on 4/25/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBBannerCell : UICollectionViewCell
+    @property (weak, readonly, nonatomic) IBOutlet UIImageView *imageView;
+@end
---
+++ Moshe/Banner/MBBannerCell.m
@@ -0,0 +1,32 @@
+//
+//  MBBannerCell.m
+//  Moshe
+//
+//  Created by Moshe Berman on 4/25/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "MBBannerCell.h"
+
+@implementation MBBannerCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+
+@end
