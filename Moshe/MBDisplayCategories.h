6c5d456147605f128dd1cd856c7697e5ba8a928a
Moshe Berman <moshberm@gmail.com>
2013-05-02 19:12:42 -0400

    Added a category header to the project.

---
+++ Moshe/MBDisplayCategories.h
@@ -0,0 +1,17 @@
+//
+//  MBDisplayCategories.h
+//  Moshe
+//
+//  Created by Moshe Berman on 5/2/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#ifndef Moshe_MBDisplayCategories_h
+#define Moshe_MBDisplayCategories_h
+
+typedef enum {
+    MBDisplayCategoryApp = 0,
+    MBDisplayCategoryCode
+} MBDisplayCategory;
+
+#endif
