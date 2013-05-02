2733e99a9e24e19483302ce575754c956500213f
Moshe Berman <moshberm@gmail.com>
2013-05-02 19:27:46 -0400

    Add a data loader class.

---
+++ Moshe/Network Loader/MBNetworkLoader.h
@@ -0,0 +1,20 @@
+//
+//  MBNetworkLoader.h
+//  Moshe
+//
+//  Created by Moshe Berman on 5/2/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Can return YES on save operations and such...
+typedef void(^MBDataLoaderCompletion)(NSData *data);
+
+@interface MBNetworkLoader : NSObject
+
+- (id)initWithURL:(NSURL *)url completion:(MBDataLoaderCompletion)completion;
+- (void) start;
+- (double)progress;
+
+@end
