fe80112cd628839fcdadaa67ba8121485d754b5e
Moshe Berman <moshberm@gmail.com>
2013-05-02 19:21:26 -0400

    Add URL Constants class to the repo.

---
+++ Moshe/MBURLConstants.h
@@ -0,0 +1,16 @@
+//
+//  MBURLConstants.h
+//  Moshe
+//
+//  Created by Moshe Berman on 5/2/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#ifndef Moshe_MBURLConstants_h
+#define Moshe_MBURLConstants_h
+
+#define kAppsURL @"http://yetanotheriphoneapp.com/?json=true"
+#define kReposURL @"https://api.github.com/users/mosheberman/repos"
+
+
+#endif
