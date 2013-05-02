1624bd5208b0c3f907bf0410b397ae9b89105a90
Moshe Berman <moshberm@gmail.com>
2013-05-02 19:13:20 -0400

    Whoops, another data model slipped through.

---
+++ Moshe/Data Source/MBRepoData.h
@@ -0,0 +1,13 @@
+//
+//  MBRepoData.h
+//  Moshe
+//
+//  Created by Moshe Berman on 5/2/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBRepoData : NSObject
+
+@end
