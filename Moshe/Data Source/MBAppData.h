d15b8d6e863806c40e3c203cff51db45d5c15024
Moshe <moshberm@gmail.com>
2014-04-13 09:05:35 -0400

    Documented MBAppData.

--- Moshe/Data Source/MBAppData.h
+++ Moshe/Data Source/MBAppData.h
@@ -26,11 +26,40 @@
 
 @interface MBAppData : NSObject
 
+/**
+ *  The app's description.
+ */
+
 @property (nonatomic, strong) NSString * appDescription;
+
+/**
+ *  This is not the iTunes Connect app ID, but a custom zero based ID.
+ */
+
 @property (nonatomic, strong) NSString * appID;
+
+/**
+ *  The URL to an app icon stored on my server.
+ */
+
 @property (nonatomic, strong) NSString * imageURL;
+
+/**
+ *  The URL to the app on the iTunes app store.
+ */
+
 @property (nonatomic, strong) NSString * appURL;
+
+/**
+ *  The name of the app.
+ */
+
 @property (nonatomic, strong) NSString * name;
+
+/**
+ *  The latest app version, as per my database.
+ */
+
 @property (nonatomic, strong) NSString * version;
 
 @end
