f459952899ba1fd4921c567e6383dcf3536906e2
Moshe <moshberm@gmail.com>
2014-04-13 11:07:58 -0400

    Renamed some classes with a new class prefix.

---
+++ Moshe/Data Source/MOSBlogPost.m
@@ -0,0 +1,13 @@
+//
+//  MBBlogPostData.m
+//  Moshe Berman
+//
+//  Created by Moshe Berman on 4/13/14.
+//  Copyright (c) 2014 Moshe Berman. All rights reserved.
+//
+
+#import "MOSBlogPost.h"
+
+@implementation MOSBlogPost
+
+@end
---
+++ Moshe/Data Source/MOSRepo.m
@@ -0,0 +1,13 @@
+//
+//  MBRepoData.m
+//  Moshe
+//
+//  Created by Moshe Berman on 5/2/13.
+//  Copyright (c) 2013 Moshe Berman. All rights reserved.
+//
+
+#import "MOSRepo.h"
+
+@implementation MOSRepo
+
+@end
