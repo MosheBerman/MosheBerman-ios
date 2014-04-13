dc06f375292c7178ea3066b0a405910acfcf19cd
Moshe <moshberm@gmail.com>
2014-04-13 08:23:24 -0400

    Basic table view implementation that does not include display of the data.

--- Moshe/MBViewController.m
+++ Moshe/MBViewController.m
@@ -6,14 +6,24 @@
 //  Copyright (c) 2013 Moshe Berman. All rights reserved.
 //
 
-#import "MBViewController.h"
+/* Frameworks. */
 #import <QuartzCore/QuartzCore.h>
-#import "MBDataSource.h"
 
-@interface MBViewController ()
+/* Some of our own headers. */
+#import "MBViewController.h"
+#import "MBDataSource.h"
+#import "MBDisplayCategories.h"
+
+/* Cell re-use identifiers. */
+static NSString *CellReuseIdentifier = @"Cell ID";
+
+/**
+ *
+ */
+
+@interface MBViewController () <UITableViewDataSource, UITableViewDelegate>
 
 @property (weak, nonatomic) IBOutlet UICollectionView *bannerView;
-@property (weak, nonatomic) IBOutlet UIPageControl *bannerPageView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *informationToggle;
 @property (weak, nonatomic) IBOutlet UITableView *informationTable;
 
@@ -23,21 +33,12 @@
 
 @implementation MBViewController
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        _dataSource = [MBDataSource new];
-    }
-    return self;
-}
-
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     
     if (self) {
-        _dataSource = [MBDataSource new];
+        _dataSource = [[MBDataSource alloc] init];
     }
     return self;
 }
@@ -45,25 +46,39 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-    CALayer *layer = [[self bannerView] layer];
-
-    [layer setShadowColor:[[UIColor blackColor] CGColor]];
-    [layer setShadowRadius:4.0];
-    [layer setShadowOffset:CGSizeMake(0, 2)];
-    [layer setShadowOpacity:1.0];
     
-    
+    /** Hook up some KVO goodness here. */
     [[self dataSource] addObserver:self forKeyPath:@"areAppsReady" options:0 context:nil];
+    [[self dataSource] addObserver:self forKeyPath:@"areBlogPostsReady" options:0 context:nil];
     [[self dataSource] addObserver:self forKeyPath:@"areReposReady" options:0 context:nil];
+    
+    /**
+     *  Reload the data source table.
+     *
+     *  TODO: Cache the downloaded data and attempt to reload from disk first.
+     */
+    
     [[self dataSource] reloadData];
 	
+    /* Where everybody knows your name. */
 	[self setTitle:@"Moshe Berman"];
+    
+    /* We need to make sure we have table cells. */
+    [[self informationTable] registerClass:[UITableViewCell class] forCellReuseIdentifier:CellReuseIdentifier];
+    
+    /* Respond to category changes by reloading the table. */
+    [[self informationToggle] addTarget:self action:@selector(reloadData) forControlEvents:UIControlEventValueChanged];
 }
 
 - (void)dealloc
 {
+    /**
+     *  LLDB calls [super dealloc] for us with ARC, but we need to remove our KVO by ourselves.
+     *  Please KVO responsibly.
+     */
+    
     [[self dataSource] removeObserver:self forKeyPath:@"areAppsReady"];
+    [[self dataSource] removeObserver:self forKeyPath:@"areBlogPostsReady"];
     [[self dataSource] removeObserver:self forKeyPath:@"areReposReady"];
 }
 
@@ -73,20 +88,151 @@
     // Dispose of any resources that can be recreated.
 }
 
-/* KVO our data loader */
+/** ----
+ *  @name KVO
+ *  ----
+ */
+
+/**
+ *  This method handles KVO for our our data loader.
+ *  See the official documentation for more on this method.
+ *
+ */
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
 {
-    if ([keyPath isEqualToString:@"areAppsReady"] || [keyPath isEqualToString:@"areReposReady"]) {
+    if ([keyPath isEqualToString:@"areAppsReady"] || [keyPath isEqualToString:@"areReposReady"] || [keyPath isEqualToString:@"areBlogPostsReady"]) {
+        
+        /**
+         *  Call on the main thread explicitly, because the network operations that change these values
+         *  may live on some other thread.
+         */
+        
         [self performSelectorOnMainThread:@selector(reloadData) withObject:nil waitUntilDone:NO];
     }
 }
 
-#pragma mark - Reload
+/** ----
+ *  @name Reloading the UI
+ *  ----
+ */
+
+/**
+ *  This method reloads the table.
+ */
 
 - (void)reloadData
 {
     [[self informationTable] reloadData];
 }
+
+/** -----
+ *  @name UITableViewDataSource
+ *  ----
+ */
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    NSInteger rowCount = 0;
+    
+    rowCount = [self expectedCount];
+    
+    /* Leave a few rows for a "no data" message. */
+    if (rowCount == 0) {
+        rowCount = 3;
+    }
+    
+    return rowCount;
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellReuseIdentifier forIndexPath:indexPath];
+    
+    /* This case is true when there is no data for a given category. */
+    if (0 == [self expectedCount])
+    {
+        /* Display a "no data" message in the middle row. */
+        if (1 == [indexPath row])
+        {
+            [[cell textLabel] setText:NSLocalizedString(@"Nothing to Display", @"The message we use for when there's no data.")];
+            [[cell textLabel] setTextColor:[UIColor colorWithWhite:0.2 alpha:0.8]];
+            [[cell textLabel] setTextAlignment:NSTextAlignmentCenter];
+        }
+        
+        /* Clear the other cells. */
+        else
+        {
+            [[cell textLabel] setText:@""]; //  Empty the text in case of recycled cells.
+        }
+        
+        //  Clear accessories.
+        [cell setAccessoryType:UITableViewCellAccessoryNone];
+    }
+    
+    /* ...otherwise, we'll show the data for the category. */
+    else
+    {
+        
+    }
+    
+    return cell;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+#pragma mark - Row Counts
+
+/**
+ *  Returns the number of rows we would need for a given 
+ *  category to display in a table without accounting
+ *  for the case of no data. We can use this to decide in
+ *  our table delegate if we have to show a regulare cell
+ *  or a "no data" cell.
+ *
+ *  @return The number of rows we'd need for the active category.
+ */
+
+- (NSInteger)expectedCount
+{
+    
+    NSInteger selectedIndex = [[self informationToggle] selectedSegmentIndex];
+    
+    NSInteger rowCount = 0;
+    
+    /* If we are displaying apps, we want enough rows for the apps. */
+    if (selectedIndex == MBDisplayCategoryApp)
+    {
+        rowCount = [[[self dataSource] apps] count];
+    }
+    
+    /* If we are displaying blog posts, we want enough rows for the blog posts. */
+    else if (selectedIndex == MBDisplayCategoryBlog)
+    {
+        rowCount = [[[self dataSource] blogPosts] count];
+    }
+    
+    /* If we are displaying code, we want enough rows for the GitHub repos. */
+    else if (selectedIndex == [[[self dataSource] repos] count])
+    {
+        rowCount = [[[self dataSource] repos] count];
+    }
+    
+    return rowCount;
+}
+
 
 @end
